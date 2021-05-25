8094daf69844b7449d41531bafb5d7a2ab8df990
Elcio <65626953+elcioroberto95@users.noreply.github.com>
2021-05-25 18:17:03 -0300

    Update app_theme.dart

--- lib/core/app_theme.dart
+++ lib/core/app_theme.dart
@@ -4,7 +4,7 @@ import 'package:memstuff/core/app_const.dart';
 final kAppTheme = ThemeData(
   primarySwatch: Colors.red,
   visualDensity: VisualDensity.adaptivePlatformDensity,
-  scaffoldBackgroundColor: Color.fromRGBO(150, 150, 120, 1),
+  scaffoldBackgroundColor: Color.fromRGBO(215, 215, 215, 1),
   buttonTheme: ButtonThemeData(
     textTheme: ButtonTextTheme.primary,
   ),
